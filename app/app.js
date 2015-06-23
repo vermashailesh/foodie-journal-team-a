b4973088905739cb8a4b090d865c6b1f787cb7cf
vaibhawkishore <kishore.vaibhaw@gmail.com>
2015-06-23 13:27:14 +0530

    Initial Task with routing done

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+angular.module('foodieJournal', [
+  'ngRoute','foodieJournal.main'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+      .when('/',{
+          templateUrl:'main/partials/_home.html',
+          controller: 'HomeController',
+      })
+      .otherwise({redirectTo: '/'});
 }]);
