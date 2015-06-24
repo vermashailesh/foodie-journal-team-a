3d0c817bc290cc645973e243810c8c91829a2043
vaibhawkishore <kishore.vaibhaw@gmail.com>
2015-06-24 18:15:51 +0530

    accoudian page

--- app/culinaris/controller/culinarisController.js
+++ app/culinaris/controller/culinarisController.js
@@ -7,5 +7,89 @@ var foodieJurnal=angular.module('foodieJournal.culinaris', [
     'ngRoute'
 ]);
 foodieJurnal.controller('CulinarisController',['$scope',function ($scope){
+    $.support.transition = false;
+         $scope.culinaris=[
+                             {
+                                 "name": "American",
+                                 "description": "One characterstic of American cooking is the fusion of multiple ethnic or regional approaches into completely new cooking style",
+                                 "duration": "3 Weeks",
+                                 "progress" : 10,
+                                 "selectedCls":"x-btn-selected",
+                                 "imagePath":"assets/images/slices/[FoodieChallenge]Asset (25).png"
+                             },
+                             {
+                                 "name": "Koriean",
+                                 "description": "One characterstic of American cooking is the fusion of multiple ethnic or regional approaches into completely new cooking style",
+                                 "duration": "1 Week",
+                                 "progress" : 20,
+                                 "imagePath":"assets/images/slices/[FoodieChallenge]Asset (1).png"
+                             },
+                             {
+                                 "name": "Italian",
+                                 "description": "One characterstic of American cooking is the fusion of multiple ethnic or regional approaches into completely new cooking style",
+                                 "duration": "1 Week",
+                                 "progress" : 20,
+                                 "imagePath":"assets/images/slices/[FoodieChallenge]Asset (1).png"
+                             },
+                             {
+                                 "name": "Mexican",
+                                 "description": "One characterstic of American cooking is the fusion of multiple ethnic or regional approaches into completely new cooking style",
+                                 "duration": "2 Weeks",
+                                 "progress": 70,
+                                 "imagePath":"assets/images/slices/[FoodieChallenge]Asset (2).png"
+                             },
+                             {
+                                 "name": "Japanese",
+                                 "description": "One characterstic of American cooking is the fusion of multiple ethnic or regional approaches into completely new cooking style",
+                                 "duration": "3 Weeks",
+                                 "progress": 0,
+                                 "imagePath":"assets/images/slices/[FoodieChallenge]Asset (3).png"
+                             }
+         ];
+    $scope.culinary=selectedCulinary();
+    function selectedCulinary(){
+        return {
+            "name": "American",
+            "description": "One characterstic of American cooking is the fusion of multiple ethnic or regional approaches into completely new cooking style",
+            "duration": "3 Weeks",
+            "progress" : 10,
+            "foods":[
+                {
+                    "id":"check-1",
+                    "title":"All-American Bacon Chees Burger",
+                    "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit, quia hic ipsam laboriosam dignissimos suscipit eligendi! Aspernatur, ad, suscipit officiis repellat consequuntur quod quibusdam sint nobis magnam voluptates veritatis?"
+                },{
+                    "id":"check-2",
+                    "title":"Treditional Americal Desserts",
+                    "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit, quia hic ipsam laboriosam dignissimos suscipit eligendi! Aspernatur, ad, suscipit officiis repellat consequuntur quod quibusdam sint nobis magnam voluptates veritatis?"
+                },
+                {
+                    "id":"check-3",
+                    "title":"San Francisco Sourdough Bread",
+                    "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit, quia hic ipsam laboriosam dignissimos suscipit eligendi! Aspernatur, ad, suscipit officiis repellat consequuntur quod quibusdam sint nobis magnam voluptates veritatis?"
+                },
+                {
+                    "id":"check-4",
+                    "title":"New York Style Buffalo Wings",
+                    "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit, quia hic ipsam laboriosam dignissimos suscipit eligendi! Aspernatur, ad, suscipit officiis repellat consequuntur quod quibusdam sint nobis magnam voluptates veritatis?"
+                },
+                {
+                    "id":"check-5",
+                    "title":"Smoked Barbecue Ribs",
+                    "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit, quia hic ipsam laboriosam dignissimos suscipit eligendi! Aspernatur, ad, suscipit officiis repellat consequuntur quod quibusdam sint nobis magnam voluptates veritatis?"
+                }
+            ],
+            "selectedCls":"x-btn-selected",
+            "imagePath":"assets/images/slices/[FoodieChallenge]Asset (25).png"
+        }
+    }
+}])
+.directive('circularProgressbar', function() {
+        return {
 
-}]);
+            scope: {
+                culinary: '=',
+            },
+            templateUrl: 'culinaris/partials/circularProgressbar.html'
+        };
+});
